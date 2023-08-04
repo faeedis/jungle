169608996358199a5cdf832e664c1e8d1897228a
saed <saedabdi3@gmail.comm>
2023-08-04 10:45:15 -0400

    cypress

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,17 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
+ 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+
+
+  resources :about, only: [:index]
+  
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
@@ -15,7 +26,10 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
+
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -72,3 +86,4 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 end
+# config/routes.rb
